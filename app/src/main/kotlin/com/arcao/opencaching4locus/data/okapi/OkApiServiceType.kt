9e15ce5ab16207b101eff717b1a0ae8a4a482aba
Arcao <arcao@arcao.com>
2017-09-22 06:43:23 +0200

    HTTPS OAuth is broken, use http protocol instead

--- app/src/main/kotlin/com/arcao/opencaching4locus/data/okapi/OkApiServiceType.kt
+++ app/src/main/kotlin/com/arcao/opencaching4locus/data/okapi/OkApiServiceType.kt
@@ -3,10 +3,11 @@ package com.arcao.opencaching4locus.data.okapi
 import com.arcao.opencaching4locus.BuildConfig
 
 enum class OkApiServiceType(val serviceName: String, val endpoint: String, val consumerKey: String = "", val consumerSecret: String = "") {
-    OPENCACHING_PL("Opencaching.PL", "https://www.opencaching.pl/okapi/", BuildConfig.OC_PL_CONSUMER_KEY, BuildConfig.OC_PL_CONSUMER_SECRET),
-    OPENCACHING_DE("Opencaching.DE", "https://www.opencaching.de/okapi/"),
-    OPENCACHING_US("Opencaching.US", "https://www.opencaching.us/okapi/"),
-    OPENCACHING_NL("Opencaching.NL", "https://www.opencaching.nl/okapi/"),
-    OPENCACHING_RO("Opencaching.RO", "https://www.opencaching.ro/okapi/"),
-    OPENCACHING_UK("Opencaching.UK", "https://www.opencaching.uk/okapi/")
+    // HTTPS OAuth is broken, use http protocol instead
+    OPENCACHING_PL("Opencaching.PL", "http://www.opencaching.pl/okapi/", BuildConfig.OC_PL_CONSUMER_KEY, BuildConfig.OC_PL_CONSUMER_SECRET),
+    OPENCACHING_DE("Opencaching.DE", "http://www.opencaching.de/okapi/"),
+    OPENCACHING_US("Opencaching.US", "http://www.opencaching.us/okapi/"),
+    OPENCACHING_NL("Opencaching.NL", "http://www.opencaching.nl/okapi/"),
+    OPENCACHING_RO("Opencaching.RO", "http://www.opencaching.ro/okapi/"),
+    OPENCACHING_UK("Opencaching.UK", "http://www.opencaching.uk/okapi/")
 }
