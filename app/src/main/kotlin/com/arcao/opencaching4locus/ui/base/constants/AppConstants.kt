4e3f4d90481cb7b5a776fbb607b984213f422ab1
Arcao <arcao@arcao.com>
2017-09-23 17:51:46 +0200

    Refactored to kotlin object class

--- app/src/main/kotlin/com/arcao/opencaching4locus/ui/base/constants/AppConstants.kt
+++ app/src/main/kotlin/com/arcao/opencaching4locus/ui/base/constants/AppConstants.kt
@@ -2,11 +2,9 @@ package com.arcao.opencaching4locus.ui.base.constants
 
 import locus.api.android.utils.LocusUtils
 
-interface AppConstants {
-    companion object {
-        const val LOCUS_MIN_VERSION = "3.8.0"
-        val LOCUS_MIN_VERSION_CODE = LocusUtils.VersionCode.UPDATE_09
+object AppConstants {
+    const val LOCUS_MIN_VERSION = "3.8.0"
+    val LOCUS_MIN_VERSION_CODE = LocusUtils.VersionCode.UPDATE_09
 
-        const val OAUTH_CALLBACK_URL = "https://martinsloup.cz/projects/opencaching4locus"
-    }
+    const val OAUTH_CALLBACK_URL = "https://martinsloup.cz/projects/opencaching4locus"
 }
