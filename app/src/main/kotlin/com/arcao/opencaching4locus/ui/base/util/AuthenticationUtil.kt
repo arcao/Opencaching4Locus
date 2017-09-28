bdf844fee1c128a84a39cc2c4c2c0000b3ff1c58
Arcao <arcao@arcao.com>
2017-09-28 20:26:25 +0200

    Use global REQUEST_SIGN_IN constant as default value

--- app/src/main/kotlin/com/arcao/opencaching4locus/ui/base/util/AuthenticationUtil.kt
+++ app/src/main/kotlin/com/arcao/opencaching4locus/ui/base/util/AuthenticationUtil.kt
@@ -4,9 +4,10 @@ import android.app.Activity
 import com.arcao.opencaching4locus.data.account.AccountManager
 import com.arcao.opencaching4locus.data.account.AccountType
 import com.arcao.opencaching4locus.ui.authentication.AuthenticationActivity
+import com.arcao.opencaching4locus.ui.base.constants.AppConstants
 
 
-fun Activity.requestSignIn(accountType: AccountType, requestCode: Int = 0): Boolean {
+fun Activity.requestSignIn(accountType: AccountType, requestCode: Int = AppConstants.REQUEST_SIGN_IN): Boolean {
     return if (!AccountManager.get(this).authenticated(accountType)) {
         startActivityForResult(AuthenticationActivity.createIntent(this, accountType), requestCode)
         true
