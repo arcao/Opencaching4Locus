99cce54a2524e25462e5aa83835a787ea69994ca
Arcao <arcao@arcao.com>
2017-10-03 19:48:10 +0200

    Use dialog theme for dashboard, added toolbar

--- app/src/main/kotlin/com/arcao/opencaching4locus/ui/dashboard/DashboardActivity.kt
+++ app/src/main/kotlin/com/arcao/opencaching4locus/ui/dashboard/DashboardActivity.kt
@@ -6,6 +6,7 @@ import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import com.arcao.opencaching4locus.R
 import com.arcao.opencaching4locus.databinding.ActivityDashboardBinding
 import com.arcao.opencaching4locus.ui.base.BaseActivity
@@ -27,6 +28,8 @@ class DashboardActivity : BaseActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard)
         binding.viewModel = viewModel
+
+        setSupportActionBar(binding.toolbar as Toolbar)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
