af995b9bbf4d6611e4afe67b461fc90cb96c9c33
Arcao <arcao@arcao.com>
2018-04-02 19:12:34 +0200

    Updated to latest dependencies

--- app/src/main/kotlin/com/arcao/opencaching4locus/data/oauth/provider/OpencachingOAuthProvider.kt
+++ app/src/main/kotlin/com/arcao/opencaching4locus/data/oauth/provider/OpencachingOAuthProvider.kt
@@ -3,16 +3,14 @@ package com.arcao.opencaching4locus.data.oauth.provider
 import com.arcao.opencaching4locus.data.okapi.OkApiServiceType
 import com.github.scribejava.core.builder.api.DefaultApi10a
 import com.github.scribejava.core.model.OAuth1RequestToken
-import com.github.scribejava.core.utils.OAuthEncoder
 
 open class OpencachingOAuthProvider(serviceType: OkApiServiceType) : DefaultApi10a() {
     private val requestTokenEndpoint = "${serviceType.endpoint}services/oauth/request_token"
     private val accessTokenEndpoint = "${serviceType.endpoint}services/oauth/access_token"
     private val authorizationUrl = "${serviceType.endpoint}services/oauth/authorize"
 
+    override fun getAuthorizationBaseUrl(): String = authorizationUrl
     override fun getRequestTokenEndpoint(): String = requestTokenEndpoint
-
     override fun getAccessTokenEndpoint(): String = accessTokenEndpoint
-
-    override fun getAuthorizationUrl(requestToken: OAuth1RequestToken): String = authorizationUrl + "?interactivity=confirm_user&oauth_token=" + OAuthEncoder.encode(requestToken.token)
+    override fun getAuthorizationUrl(requestToken: OAuth1RequestToken): String = super.getAuthorizationUrl(requestToken) + "&interactivity=confirm_user"
 }
